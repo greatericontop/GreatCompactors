98cbf6f568b0fb3d58c80e07d261b3c5b9493a76
greatericontop <greateric@protonmail.com>
2024-08-31 16:11:00 -0400

    fix NPEs

--- src/main/java/io/github/greatericontop/greatcompactors/compactoritem/CompactingListener.java
+++ src/main/java/io/github/greatericontop/greatcompactors/compactoritem/CompactingListener.java
@@ -33,6 +33,7 @@ public class CompactingListener implements Listener {
         // Get the recipes we're going to try to make
         List<CompactorRecipe> toMake = new ArrayList<>();
         for (ItemStack stack : plugin.playerdataManager.getCompactorInventory(player.getUniqueId())) {
+            if (stack == null || stack.getType() == Material.AIR)  continue;
             toMake.addAll(plugin.recipes.get(stack.getType()));
         }
 
